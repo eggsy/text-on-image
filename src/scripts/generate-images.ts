557752c06d160c54b6de8c23ac14545edc028b3f
Abdulbaki Dursun <eggsydev@gmail.com>
2023-12-01 00:46:35 +0300

    fix: breaking changes in listr2

--- src/scripts/generate-images.ts
+++ src/scripts/generate-images.ts
@@ -2,6 +2,7 @@ import { createCanvas, loadImage } from "canvas";
 import { yellow } from "ansi-colors";
 import { writeFileSync } from "fs";
 import { Listr } from "listr2";
+import { ListrEnquirerPromptAdapter } from "@listr2/prompt-adapter-enquirer";
 
 // Scripts
 import GeneratePdf from "./generate-pdf";
@@ -39,126 +40,124 @@ const tasks = new Listr<Ctx>([
   {
     title: "Starting image manipulation...",
     task: (context, t) => {
-      return t.newListr(
-        [
-          {
-            title: "Creating canvas...",
-            task: async (_, task) => {
-              const canvas = createCanvas(
-                config.image.sizes.width,
-                config.image.sizes.height
-              );
-              const ctx = canvas.getContext("2d");
+      return t.newListr([
+        {
+          title: "Creating canvas...",
+          task: async (_, task) => {
+            const canvas = createCanvas(
+              config.image.sizes.width,
+              config.image.sizes.height
+            );
+            const ctx = canvas.getContext("2d");
 
-              /* Get the absolute path of output directory and image */
-              const outputDirectory = getOutputDirectory();
-              const imageFile = getBaseDirectory(config.image.path);
+            /* Get the absolute path of output directory and image */
+            const outputDirectory = getOutputDirectory();
+            const imageFile = getBaseDirectory(config.image.path);
 
-              /* Load target image and start generating images */
-              const image = await loadImage(imageFile);
+            /* Load target image and start generating images */
+            const image = await loadImage(imageFile);
 
-              /* Apply values to Listr context */
-              context.canvas = {
-                self: canvas,
-                ctx,
-              };
+            /* Apply values to Listr context */
+            context.canvas = {
+              self: canvas,
+              ctx,
+            };
 
-              context.outputDirectory = outputDirectory;
-              context.image = image;
+            context.outputDirectory = outputDirectory;
+            context.image = image;
 
-              task.title = "Created canvas";
-            },
+            task.title = "Created canvas";
           },
-          {
-            title: "Generating images...",
-            task: (_, task) => {
-              const amount = config?.image?.amount || 0;
+        },
+        {
+          title: "Generating images...",
+          task: (_, task) => {
+            const amount = config?.image?.amount || 0;
 
-              for (let i = 1; i < amount + 1; i++) {
-                /* Get values from Listr context */
-                const { ctx, self: canvas } = context.canvas;
-                const { image, outputDirectory } = context;
+            for (let i = 1; i < amount + 1; i++) {
+              /* Get values from Listr context */
+              const { ctx, self: canvas } = context.canvas;
+              const { image, outputDirectory } = context;
 
-                const coloredText = yellow(`${i + 1}/${amount}`);
-                task.title = `Generating images (${coloredText})...`;
+              const coloredText = yellow(`${i + 1}/${amount}`);
+              task.title = `Generating images (${coloredText})...`;
 
-                ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+              ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-                if (typeof config.text.position === "object") {
-                  ctx.font = `${config.text.size || "24"}px Inter black`;
-                  ctx.fillStyle = "white";
-                  ctx.textAlign = config.text?.align || "left";
+              if (typeof config.text.position === "object") {
+                ctx.font = `${config.text.size || "24"}px Inter black`;
+                ctx.fillStyle = "white";
+                ctx.textAlign = config.text?.align || "left";
 
-                  ctx.fillText(
-                    `${i}`,
-                    config.text.position.x,
-                    config.text.position.y
-                  );
-                }
-
-                writeFileSync(
-                  `${outputDirectory}/${config.image.fileName.replace(
-                    "{1}",
-                    String(i)
-                  )}`,
-                  canvas.toBuffer()
-                );
-
-                ctx.clearRect(
-                  canvas.height,
-                  canvas.width,
-                  canvas.width,
-                  canvas.height
+                ctx.fillText(
+                  `${i}`,
+                  config.text.position.x,
+                  config.text.position.y
                 );
               }
 
-              task.title = `Created and saved ${amount} images`;
-              t.title = "Finished image manipulation";
-            },
+              writeFileSync(
+                `${outputDirectory}/${config.image.fileName.replace(
+                  "{1}",
+                  String(i)
+                )}`,
+                canvas.toBuffer()
+              );
+
+              ctx.clearRect(
+                canvas.height,
+                canvas.width,
+                canvas.width,
+                canvas.height
+              );
+            }
+
+            task.title = `Created and saved ${amount} images`;
+            t.title = "Finished image manipulation";
           },
-        ],
-        { rendererOptions: { collapse: false } }
-      );
+        },
+      ]);
     },
   },
   {
     title: "Finishing...",
     task: async (_, t) => {
-      return t.newListr(
-        [
-          {
-            title: "Save to PDF?...",
-            task: async (__, task) => {
-              const savePdf = await task.prompt<boolean>({
-                type: "Confirm",
-                message: "Save to PDF?",
-              });
+      return t.newListr([
+        {
+          title: "Save to PDF?...",
+          task: async (__, task) => {
+            const prompt = task.prompt(ListrEnquirerPromptAdapter);
 
-              if (savePdf === true) {
-                await GeneratePdf.run();
-                task.title = "Saved PDF file";
-              }
-            },
+            const savePdf = await prompt.run<boolean>({
+              type: "Confirm",
+              message: "Save to PDF?",
+            });
+
+            if (savePdf === true) {
+              await GeneratePdf.run();
+              task.title = "Saved PDF file";
+            }
           },
-          {
-            title: "Delete output directory?",
-            task: async (__, task) => {
-              const removeOutput = await task.prompt<boolean>({
-                type: "Confirm",
-                message: "Delete output directory?",
-              });
+        },
+        {
+          title: "Delete output directory?",
+          task: async (__, task) => {
+            const prompt = task.prompt(ListrEnquirerPromptAdapter);
 
-              if (removeOutput) {
-                removeDirectory(config.outputDirectory);
-                task.title = "Deleted output directory";
+            const removeOutput = await prompt.run<boolean>({
+              type: "Confirm",
+              message: "Delete output directory?",
+            });
 
-                t.title = "Finished all tasks";
-              }
-            },
+            if (removeOutput) {
+              removeDirectory(config.outputDirectory);
+              task.title = "Deleted output directory";
+
+              t.title = "Finished all tasks";
+            }
           },
-        ],
-        { rendererOptions: { collapse: false } }
-      );
+        },
+      ]);
     },
   },
 ]);
